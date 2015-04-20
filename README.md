4e38930714a21f0cb5c1b8e723807fdc7fb3a80a
peterjfriend08 <yss265@nyu.edu>
2015-04-19 23:52:08 -0400

    get all movie info

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # SEA-final-project
 Building up movie seach engine plus customized recommendation system
+
+#tomatoCrawler
+![alt tag](./constant/tomatoCrawler.png)
+Will fetch rotten tomato website and save the following pickles:
+./constants/Movie_dict and ./constants/Review_dict
+Now we have:
+- 250 movie to search
+- 1718 movieIDs returned
--- src/tomatoCrawler.py
+++ src/tomatoCrawler.py
@@ -5,7 +5,7 @@ import hashlib
 import socket
 import getpass
 import urllib,re, zlib,time
-
+import pickle
 import json
 
 from tornado.httpclient import AsyncHTTPClient
@@ -13,14 +13,30 @@ from tornado.httpclient import HTTPClient
 from tornado import gen
 from tornado.options import define, options
 
+####################################################
+#
+# This module extracts movie IDs and Reviews from RottonTomato:
+# Usage:
+# python tomatoCrawler.py   then will save Movie_dict and Review_dict as pickle files
+# 
+# Review_dict[movieIDs].keys() --> [u'ratings', u'links', u'title', u'critics_consensus', u'release_dates', u'abridged_cast', u'synopsis', u'mpaa_rating', u'year', u'alternate_ids', u'posters', u'runtime', u'id']
+# Review_dict[movieIDs].keys() --> [u'reviews', u'total', u'link_template', u'links']
+#
+####################################################
+
+
 ## Some constant info
+import movieNameInventory as Inv 
 Movies = ["divergent", "toy story 2", "hunger game", "INTERSTELLAR"]
+Movies = Inv.getMovieNames()
+print Movies
 IDs = [] # collect return ids. Note: will be more than Movies list (e.g. will get two returns for divergent)
 Movie_dict = {} # key is movie ID, value is a dict of the movie search return
 Review_dict = {} # key is the movie ID
 MovieSearch = "http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=x6usx7bn33cdn9vverg9f2v7&q="
 ReviewSearch = "http://api.rottentomatoes.com/api/public/v1.0/movies/#ID HERE#/reviews.json?apikey=x6usx7bn33cdn9vverg9f2v7&review_type=all&page_limit=50"
-# ReviewSearch = "http://api.rottentomatoes.com/api/public/v1.0/movies/771315918/reviews.json?apikey=x6usx7bn33cdn9vverg9f2v7&review_type=all&page_limit=50"
+RelationSearch = "http://api.rottentomatoes.com/api/public/v1.0/movies/#ID HERE#/similar.json?apikey=x6usx7bn33cdn9vverg9f2v7"
+
 
 def _load_json_from_url(url):
   """
@@ -45,13 +61,23 @@ def tomatoMovieSearch():
 	global Movies, IDs, Movie_dict, Review_dict
 	for movie in Movies:
 		toFetch = MovieSearch + movie.replace(" ", "%20")
-		http_client = AsyncHTTPClient()
-		response = yield http_client.fetch(toFetch)
-		data = eval(response.body)
-		for i in range(data['total']):			
-			IDs.append(data['movies'][i]['id'])
-			Movie_dict[data['movies'][i]['id']] = data['movies'][i]
-
+		print bcolors.OKGREEN + "Fetching: " + toFetch + bcolors.ENDC
+		data = _load_json_from_url(toFetch)		
+		
+		for i in range(data['total']):
+			try:
+				IDs.append(data['movies'][i]['id'])
+				Movie_dict[data['movies'][i]['id']] = data['movies'][i]
+			except:
+				pass
+		time.sleep(0.5)
+		# for i in range(data['total']):
+		# 	IDs.append(data['movies'][i]['id'])
+		# 	Movie_dict[data['movies'][i]['id']] = data['movies'][i]
+		# print "fetching: " + toFetch
+		# http_client = AsyncHTTPClient()
+		# response = yield http_client.fetch(toFetch)
+		# data = eval(response.body)
 
 @gen.coroutine
 def tomatoReviewSeach():
@@ -60,11 +86,12 @@ def tomatoReviewSeach():
 	for myid in IDs:
 		pageNum=1
 		toFetch = "http://api.rottentomatoes.com/api/public/v1.0/movies/%s/reviews.json?apikey=x6usx7bn33cdn9vverg9f2v7&page_limit=50&review_type=all&page=%s" %(myid,pageNum)
-		print toFetch
+		# print toFetch
 		data = _load_json_from_url(toFetch)		
-		time.sleep(1)		
-		print "myid: " + str(myid) + "\t" + "total: " + str(data['total'])
+		time.sleep(1)				
+		print bcolors.OKGREEN + "MovieID: " + str(myid) + "\t" + "Title: " + Movie_dict[myid]['title'] + "\t" + "Total Reviews: " + str(data['total']) + bcolors.ENDC
 		
+		#API only provide 50 reviews per page, so fetching more pages if total review is more than 50
 		if(data['total']>50):
 			loop = int((data['total']+49)/50)-1
 			for i in range(loop):
@@ -83,16 +110,34 @@ def tomatoReviewSeach():
 
 
 if __name__ == "__main__":        
+	import color
+	bcolors = color.bcolors()
+
+	print bcolors.HEADER + "====== START: tomatoMovieSearch ======" + bcolors.ENDC
 	tornado.ioloop.IOLoop.current().run_sync(tomatoMovieSearch)
+	print bcolors.OKBLUE + "Number of IDs: " +  str(len(IDs)) + bcolors.ENDC	
+
+	# for myid in IDs:
+	# 	print "myid: " + myid 		
+	# for key in Movie_dict.keys():
+	# 	print str(key) + "\t" + Movie_dict[key]['title'] + "\n" + Movie_dict[key]['synopsis']
 	
-	for myid in IDs:
-		print "myid: " + myid 		
-	for key in Movie_dict.keys():
-		print str(key) + "\t" + Movie_dict[key]['title'] + "\n" + Movie_dict[key]['synopsis']
-
+	print bcolors.HEADER + "====== START: tomatoReviewSeach ======" + bcolors.ENDC
 	tornado.ioloop.IOLoop.current().run_sync(tomatoReviewSeach)
+	# for key in Review_dict.keys():
+	# 	print str(key) + "\t" + Movie_dict[key]['title'] + "\n" + str(len(Review_dict[key]['reviews']))	
 
-	for key in Review_dict.keys():
-		print str(key) + "\t" + Movie_dict[key]['title'] + "\n" + str(len(Review_dict[key]['reviews']))
+	print bcolors.HEADER + "====== Saving PICKLE ======" + bcolors.ENDC
+	fileObj = open('../constants/Movie_dict', 'w')
+	pickle.dump(Movie_dict, fileObj)
+	fileObj.close()    
 
+	fileObj = open('../constants/Review_dict', 'w')
+	pickle.dump(Review_dict, fileObj)
+	fileObj.close()    
+
+
+	print bcolors.OKGREEN + "Keys in Movie_dict[MovieID]\n" + bcolors.ENDC + str(Movie_dict[Movie_dict.keys()[0]].keys())
+	print bcolors.OKGREEN + "Keys in Review_dict[MovieID]\n" + bcolors.ENDC + str(Review_dict[Review_dict.keys()[0]].keys())
+	
 
