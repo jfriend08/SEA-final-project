95c2db21ad9d89ffcd1b597a4aa33fc261df29b2
Hung-Ting Wen <htw230@nyu.edu>
2015-04-22 17:56:28 -0400

    Barebone of master/worker

---
+++ fs/master.py
@@ -0,0 +1,17 @@
+
+class FSMaster(object):
+  def __init__(self):
+    pass
+
+  def create(self):
+    raise NotImplementedError
+
+  def get(self):
+    raise NotImplementedError
+
+  def set(self):
+    raise NotImplementedError
+
+  def remove(self):
+    raise NotImplementedError
+
---
+++ fs/workers.py
@@ -0,0 +1,13 @@
+
+class FSWorker(object):
+  def __init__(self):
+    pass
+
+  def get(self):
+    raise NotImplementedError
+
+  def set(self):
+    raise NotImplementedError
+
+  def remove(self):
+    raise NotImplementedError
